e217ffc65851f28f393b578d0ee71e2bb032b59f
Frank-197 <frankomosh197@gmail.com>
2022-11-04 12:59:58 +0300

    adding launches

--- server/src/app.js
+++ server/src/app.js
@@ -1,13 +1,24 @@
+const path = require('path');
 const express = require('express');
 const cors = require('cors');
-const planetsRouter = require('./routes/planets/planets.router');
+const morgan = require('morgan');
+
+const api = require('./routes/api');
 
 const app = express();
 
 app.use(cors({
-    origin: 'http://localhost:3001',
+  origin: 'http://localhost:3000',
 }));
+app.use(morgan('combined'));
+
 app.use(express.json());
-app.use(planetsRouter)
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
+app.use('/v1', api);
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+});
 
 module.exports = app;
